40929f8b5ac277934a8db2facb1694962b91c751
dmytro-ulianov <dmytro.ulianov16@gmail.com>
2020-04-08 00:01:57 +0300

    refactor: use inline exports

--- src/constructors.ts
+++ src/constructors.ts
@@ -1,23 +1,26 @@
-import {Resource, Initial, Loading} from 'types'
+import {Resource, Initial, Loading, Success, Failure} from './types'
 
-enum tag {
+export enum tag {
   initial = 'initial',
   loading = 'loading',
   success = 'success',
   failure = 'failure',
 }
 
-const initial: Initial = {tag: tag.initial}
-const loading: Loading = {tag: tag.loading}
-const success = <D>(value: D): Resource<D> => ({tag: tag.success, value})
-const failure = <E>(error: E): Resource<any, E> => ({tag: tag.failure, error})
-const of = <D, E = any>(d: D): Resource<D, E> => success(d)
+export const initial: Initial = {tag: tag.initial}
+export const loading: Loading = {tag: tag.loading}
+export const success = <D>(value: D): Success<D> => ({tag: tag.success, value})
+export const failure = <E>(error: E): Failure<E> => ({tag: tag.failure, error})
 
-const fromNullable = <D, E = any>(a: D | null | undefined): Resource<D, E> => {
+export const of = <D, E = any>(d: D): Resource<D, E> => success(d)
+
+export const fromNullable = <D, E = any>(
+  a: D | null | undefined,
+): Resource<D, E> => {
   return a == null ? initial : of(a)
 }
 
-const tryCatch = <D, E = any>(f: () => D): Resource<D, E> => {
+export const tryCatch = <D, E = any>(f: () => D): Resource<D, E> => {
   try {
     return of(f())
   } catch (e) {
@@ -25,23 +28,10 @@ const tryCatch = <D, E = any>(f: () => D): Resource<D, E> => {
   }
 }
 
-const toNullable = <D, E>(resource: Resource<D, E>): D | null => {
+export const toNullable = <D, E>(resource: Resource<D, E>): D | null => {
   return resource.tag === 'success' ? resource.value : null
 }
 
-const toUndefined = <D, E>(resource: Resource<D, E>): D | undefined => {
+export const toUndefined = <D, E>(resource: Resource<D, E>): D | undefined => {
   return resource.tag === 'success' ? resource.value : undefined
-}
-
-export {
-  tag,
-  initial,
-  loading,
-  success,
-  failure,
-  of,
-  fromNullable,
-  tryCatch,
-  toNullable,
-  toUndefined,
 }
